17412d824838164849787afaa849f9eb4aef54a2
Mohamed Elfergouch <elfergouchmohamed@gmail.com>
2024-01-02 09:11:32 +0100

    update vm disk

--- AzureTerraform/main.tf
+++ AzureTerraform/main.tf
@@ -74,7 +74,7 @@ resource "azurerm_linux_virtual_machine" "Jenkins-vm" {
     name                 = "Jenkins-disk"
     caching              = "ReadWrite"
     storage_account_type = "StandardSSD_LRS"
-    disk_size_gb = 60
+    disk_size_gb = 70
   }
 
   source_image_reference {
